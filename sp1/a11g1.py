ca330ac4d6818a5e2741c46edaa7caa6dfe10512
epontoni <pontoni.emanuel@gmail.com>
2022-06-05 00:47:09 -0300

    issue #5 - Creación de la función ing2s

--- sp1/a11g1.py
+++ sp1/a11g1.py
@@ -3,7 +3,12 @@
 # Importamos la función bienvenida
 from funciones.bienvenida import bienvenida
 from funciones.ing2i import ing2i
+from funciones.ing2s import ing2s
 
 bienvenida()
+
 enteros = ing2i()
 print('Los enteros intresados son: ', enteros[0], enteros[1])
+
+cadenas = ing2s()
+print('Los strings intresados son: ', cadenas[0], cadenas[1])
---
+++ sp1/funciones/ing2s.py
@@ -0,0 +1,4 @@
+def ing2s():
+    s1 = input()
+    s2 = input()
+    return [s1, s2]
