841735d54ed9d229cfe1ef30dff7c21b22dd2a5f
epontoni <pontoni.emanuel@gmail.com>
2022-06-05 00:44:09 -0300

    issue #4 - Creación de la función ing2i

--- sp1/a11g1.py
+++ sp1/a11g1.py
@@ -2,5 +2,8 @@
 
 # Importamos la función bienvenida
 from funciones.bienvenida import bienvenida
+from funciones.ing2i import ing2i
 
 bienvenida()
+enteros = ing2i()
+print('Los enteros intresados son: ', enteros[0], enteros[1])
---
+++ sp1/funciones/ing2i.py
@@ -0,0 +1,5 @@
+#permita el ingreso de 2 valores enteros.
+def ing2i():
+    i1 = int(input("Ingrese el primer entero: "))
+    i2 = int(input("Ingrese el segundo entero: "))
+    return [i1, i2]
